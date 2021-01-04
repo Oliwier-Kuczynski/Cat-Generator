6f77c1cea071ef7fe355562f67e9fce1a87cfb28
Oliwier965 <olikucz@vp.pl>
2021-01-04 16:50:44 +0100

    title color changed

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function generate() {
     let catContainer = document.querySelector('.cat-container');
     let img = document.createElement('img');
     img.src = "http://thecatapi.com/api/images/get?format=src&type=gif&timestamp=";
+    img.classList.add('img');
     catContainer.appendChild(img);
 };
